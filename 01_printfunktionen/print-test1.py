e64503279c9d4584c1890fd94ca32a2c1484bd3b
Jacob Debel <jacobdebel@gmail.com>
2022-08-12 15:18:01 +0200

    Fjernede to unødvendige py-filer

--- 01_printfunktionen/print-test1.py
+++
@@ -1,8 +0,0 @@
-import arcade
-arcade.open_window(600, 600, "Mit nye arcadebvindue")
-
-arcade.start_render()
-arcade.draw_circle_filled(300, 300, 50,arcade.color.RED)
-arcade.draw_ellipse_outline(400, 500, 100, 50, arcade.color.BLUE, 30)
-arcade.finish_render()
-arcade.run()
--- 01_printfunktionen/test_af_printfunktionen.py
+++
@@ -1,1 +0,0 @@
-print("Her med jer. Saa er vi i gang!")
